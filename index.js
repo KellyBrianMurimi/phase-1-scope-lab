3bf74a6a016a8c30f7b61d08581dd0d5b21bd431
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2023-09-26 09:30:04 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+//Write your solution in this file! 
+var customerName = "bob";
+ 
+function upperCaseCustomerName() {
+    return customerName=customerName.toUpperCase();
+    } 
+function setBestCustomer() {
+     bestCustOmer="not bob"; 
+    } 
+function overwritesBestCustomer() {
+     bestCustomer="maybe bob";
+}
+
+const leastFavoriteCustomer = "Brian";
+//Function that attempts to change the constant 
+function changeLeastFavoriteCustomer(newCustomer) {
+    leastFavoriteCustomer = newCustomer;
+    //This will result in an error
+    //Attempting to change a constant will result in an error
+}
